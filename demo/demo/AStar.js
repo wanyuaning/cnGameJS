477995e2adde8d4ec11a36067d46fee28c5a6320
ewan <wane@qq.com>
2021-02-09 21:00:25 +0800

    v

--- demo/demo/AStar.js
+++ demo/demo/AStar.js
@@ -1,97 +1,89 @@
-﻿var aStarManager = (function() {     
-    var keyMapOpen = {},
-        listClose = [],
-        path = []
-    var isInOpen = function(point) {
-        return keyMapOpen[point.key] ? true : false
-    }
-    var isInClose = function(point) {
-        for (var i = 0, len = listClose.length; i < len; i++) {
-            if (listClose[i] == point) return true
-        }
-        return false
-    }
-    /*  获取开始列表中F值最小的结点 */
-    var getPointWithMinF = function() {
-        var minPoint
-        for (var i in keyMapOpen) {
-            if (!minPoint || keyMapOpen[i].F < minPoint.F) minPoint = keyMapOpen[i]
-        }
-        return minPoint
-    }
-    var addToClose = function(point) {
-        console.log('====',point.key);
-        
-        if (isInOpen(point)) deleteFromOpen(point)
-        listClose.push(point)
-    }
-    var deleteFromOpen = function(point) {
-        delete keyMapOpen[point.key]
-    }
-    /*  保存路径    */
-    var savePath = function(point) {
-        path.unshift(point)
-        if (point.parent) arguments.callee(point.parent)
-    }
-    /*  判断该点是否围墙    */
-    var isWall = function(point, wallValueArr) {
-        for (var i = 0, len = wallValueArr.length; i < len; i++) {
-            if (wallValueArr[i] == point.type) return true
-        }
-        return false
-    }
-    /*  开始寻路    */
-    var handle = function(point) {
-        var beginX = point.x - 1,
-            beginY = point.y - 1,
-            endX = point.x + 1,
-            endY = point.y + 1,
-            pointsArr = this.pointsArr   
-
-        addToClose(point) //把传入的当前结点放进关闭列表
-
-        for (var i = beginY; i <= endY; i++) {//遍历传入结点的四周的结点
-            for (var j = beginX; j <= endX; j++) {
-                if (pointsArr[i] && pointsArr[i][j] && !isInClose(pointsArr[i][j])) {//如果该位置结点存在并且不是传入的结点
-                    if (!isWall(pointsArr[i][j], this.wallValueArr)) {//不是障碍物
-                        //拐角规则，如果检测某点四周的点时，该点和四周上某点之间隔着一个障碍物，则忽略该点，暂不添加到开始列表
-                        if (isWall(pointsArr[i][point.x], this.wallValueArr) || isWall(pointsArr[point.y][j], this.wallValueArr)) continue
-
-                        var G = pointsArr[i][j].caculateG(point)
-                        if (isInOpen(pointsArr[i][j])) {//如果在开始列表，则根据传入的指定点计算是否改变G值
-                            if (pointsArr[i][j].G > G) {//如果该结点的G值大于由传入结点计算出的G值，则该结点父指针指向传入结点
-                                pointsArr[i][j].G = G
-                                pointsArr[i][j].updateF()
-                                pointsArr[i][j].parent = point
+﻿var aStarManager = (function() { 
+    return {
+        isInOpen(point){return !!this.keyMapOpen[point.key]},
+        isInClose(point){return !!this.keyMapClose[point.key]},
+        /*  获取开始列表中F值最小的结点 */
+        getPointWithMinF: function() {
+            var minPoint
+            for (var i in this.keyMapOpen) {
+                if (!minPoint || this.keyMapOpen[i].F < minPoint.F) minPoint = this.keyMapOpen[i]
+            }
+            return minPoint
+        },
+        addToClose: function(point) {
+            if (this.isInOpen(point)) this.deleteFromOpen(point)
+            this.keyMapClose[point.key] = point
+        },
+        deleteFromOpen: function(point) {
+            delete this.keyMapOpen[point.key]
+        },
+        /*  保存路径    */
+        savePath: function(point) {
+            this.path.unshift(point)
+            if (point.parent) arguments.callee.call(this, point.parent)
+        },
+        /*  判断该点是否围墙    */
+        isWall: function(point, wallValueArr) {
+            for (var i = 0, len = wallValueArr.length; i < len; i++) {
+                if (wallValueArr[i] == point.type) return true
+            }
+            return false
+        },
+        /* 寻路 */
+        handle: function(point) {
+            var beginX = point.x - 1,
+                beginY = point.y - 1,
+                endX = point.x + 1,
+                endY = point.y + 1,
+                pointsArr = this.pointsArr   
+    
+            this.addToClose(point) //把传入的当前结点放进关闭列表
+    
+            for (var i = beginY; i <= endY; i++) {//遍历传入结点的四周的结点
+                for (var j = beginX; j <= endX; j++) {                
+                    if (pointsArr[i] && pointsArr[i][j] && !this.isInClose(pointsArr[i][j])) {//如果该位置结点存在并且不是传入的结点
+                        var pointer = pointsArr[i][j]
+                        if (!this.isWall(pointer, this.wallValueArr)) {//不是障碍物
+                            //拐角规则，如果检测某点四周的点时，该点和四周上某点之间隔着一个障碍物，则忽略该点，暂不添加到开始列表
+                            if (this.isWall(pointsArr[i][point.x], this.wallValueArr) || this.isWall(pointsArr[point.y][j], this.wallValueArr)) continue
+    
+                            var G = pointer.caculateG(point)
+                            if (this.isInOpen(pointer)) {//如果在开始列表，则根据传入的指定点计算是否改变G值
+                                if (pointer.G > G) {//如果该结点的G值大于由传入结点计算出的G值，则该结点父指针指向传入结点
+                                    pointer.G = G
+                                    pointer.updateF()
+                                    pointer.parent = point
+                                }
                             }
+                            else if (!this.isInClose(pointer)) {//如果不在开始列表也不在关闭列表，则根据传入的指定点计算该点G值，并把该结点父节点指针指向传入结点
+                                pointer.G = G
+                                pointer.updateH(this.endPoint)
+                                pointer.updateF()
+                                pointer.parent = point
+                                this.keyMapOpen[j + '' + i] = pointer //添加到开始列表
+                            }
+                        } else {//如果是障碍物，则添加到关闭列表
+                            this.addToClose(pointer)
                         }
-                        else if (!isInClose(pointsArr[i][j])) {//如果不在开始列表也不在关闭列表，则根据传入的指定点计算该点G值，并把该结点父节点指针指向传入结点
-                            pointsArr[i][j].G = G
-                            pointsArr[i][j].updateH(this.endPoint)
-                            pointsArr[i][j].updateF()
-                            pointsArr[i][j].parent = point
-                            keyMapOpen[j + '' + i] = pointsArr[i][j] //添加到开始列表
-                        }
-                    } else {//如果是障碍物，则添加到关闭列表
-                        addToClose(pointsArr[i][j])
                     }
                 }
             }
-        }
-
-        if (point != this.endPoint) {//如果被选中结点不是终点结点
-            var nextPoint = getPointWithMinF() //获取开始列表中F值最小的结点
-            arguments.callee.call(this, nextPoint) //递归重复上述操作
-        } else {
-            savePath(point) //如果当前结点是终点结点并且该结点被添加到关闭列表，则保存路径，寻路结束
-        }
-    }
-
-    return {
+    
+            if (point != this.endPoint) {//如果被选中结点不是终点结点
+                var nextPoint = this.getPointWithMinF() //获取开始列表中F值最小的结点
+                arguments.callee.call(this, nextPoint) //递归重复上述操作
+            } else {
+                this.savePath(point) //如果当前结点是终点结点并且该结点被添加到关闭列表，则保存路径，寻路结束
+            }
+        },
         install(options){
             this.pointsArr = []
             this.pointsMap = {}
             this.wallValueArr = options.wallValueArr || []
+
+            this.keyMapOpen = {}
+            this.keyMapClose = {}
+            this.path = []
 
             var map = options.mapMatrix || []
             for (var r = 0; r < map.length; r++) {
@@ -106,9 +98,9 @@
             }
         },
         init: function(startPointPos, endPointPos) {
-            keyMapOpen = {}
-            listClose = []
-            path = []
+            this.keyMapOpen = {}
+            this.keyMapClose = {}
+            this.path = []
             
             for (var i in this.pointsMap) {
                 this.pointsMap[i].parent = undefined
@@ -121,11 +113,11 @@
         },
         getPath: function(startPointPos, endPointPos) {
             var targetPointer = this.pointsArr[endPointPos[1]][endPointPos[0]]            
-            if (isWall(targetPointer, this.wallValueArr)) return
+            if (this.isWall(targetPointer, this.wallValueArr)) return
 
             this.init(startPointPos, endPointPos)
-            handle.call(this, this.startPoint)
-            return path
+            this.handle(this.startPoint)
+            return this.path
         }
     }
 })();
